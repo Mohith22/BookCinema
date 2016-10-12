5278ef9ca9576c2a6864e5d99ed4bc63e80ec4b4
Mohith Damarapati <mohithdamarapati@gmail.com>
2016-10-12 18:10:53 +0530

    Query Page Done

--- query.php
+++ query.php
@@ -42,13 +42,13 @@ if(!isset($_SESSION['admin'])){
         <span class="icon-bar"></span>
         <span class="icon-bar"></span> 
       </button>
-      <a class="navbar-brand" href="#">BOOK MOVIE</a>
+      <a class="navbar-brand" href="#">Queries</a>
     </div>
 
     <ul class="nav navbar-nav navbar-right collapse navbar-collapse" id="myNavbar">
-      <li class="active"><a href="admin.php">Home</a></li>
+      <li><a href="admin.php">Home</a></li>
       <li><a href="addmovie.php">Add Movie</a></li>
-      <li><a href="query.php">Queries</a></li>  
+      <li class="active"><a href="query.php">Queries</a></li>  
       <li><a href="logout.php">LogOut</a></li>
     </ul>
   </div>
@@ -59,9 +59,29 @@ if(!isset($_SESSION['admin'])){
  <br>
  <br>
 
-   <div class="container">
-  <b><p class="about"> Welcome Admin </p> </b>
-  </div>
+    <?php 
+    $connection = mysql_connect("localhost", "root", ""); // Establishing Connection with Server
+    $db = mysql_select_db("BookMovie", $connection);
+
+      $result = mysql_query("SELECT * FROM query") or die(mysql_error());
+    while ($row = mysql_fetch_array($result)) {
+  echo '<div class="row">';
+  echo '<div class="col-md-2" > </div>';
+  echo '<div class="col-md-7">';
+  echo "<div class='panel panel-primary '>";
+  echo " <div class='panel-body'>";
+  echo "<br>";
+  echo "<b><h1>".$row['Name']."</h1></b>"."<br> ";
+  echo "</a>";
+  echo "<b><p class='about1'>".$row['Email']."</p></b>";
+  echo "<b><p class='about1'>".$row['Message']."</p></b>";
+  echo "</div> </div> </div>";
+  echo "<br><br><br><br>";
+  echo "</div>";
+  echo '<div class="col-md-2"> </div>';
+
+}
+    ?>
 
  
     </body>
